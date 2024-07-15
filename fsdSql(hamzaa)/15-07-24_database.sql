49a3e6be3c2a3bf2826dac39b23b56c134a2003b
Hamzaa605 <hs073545@gmail.com>
2024-07-15 09:03:57 +0530

    8th commit

---
+++ fsdSql(hamzaa)/15-07-24_database.sql
@@ -0,0 +1,11 @@
+-- To create database
+create database Hamzaa_Shaikh;
+-- To see all database
+show databases;
+-- To select database(use database_name)
+use Hamzaa_Shaikh;
+-- To see all tables in database
+show tables;
+-- To delete database(drop databade database_name)
+drop database hamzaa_shaikh;
+
